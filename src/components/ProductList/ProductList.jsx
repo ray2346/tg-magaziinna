f75b52296dede9db9039c2aa8570a56ea105d892
Sts <tooshka4@yandex.ru>
2022-10-28 15:05:29 +0300

    Ебнул Сервак
    Signed-off-by: Sts <tooshka4@yandex.ru>

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -29,7 +29,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://91.105.198.101:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
